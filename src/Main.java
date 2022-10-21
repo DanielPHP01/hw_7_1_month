918de84e36b6e1af99b8de071c0bca5041b87a3f
DanielPHP01 <danielmokushov01@gmail.com>
2022-10-21 22:20:46 +0600

    utf-8

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
 
     public static void main(String[] args) {
-        Magic magic = new Magic(200, 50, " Магия ");
-        Warrior warrior = new Warrior(300, 20, " Критичиский урон ");
-        Medic medic = new Medic(100, 5, 20, " Супер лечение ");
+        Magic magic = new Magic(200, 50, " РњР°РіРёСЏ ");
+        Warrior warrior = new Warrior(300, 20, " РљСЂРёС‚РёС‡РёСЃРєРёР№ СѓСЂРѕРЅ ");
+        Medic medic = new Medic(100, 5, 20, " РЎСѓРїРµСЂ Р»РµС‡РµРЅРёРµ ");
         Hero[] heroes = {medic, warrior, magic};
         for (int i = 0; i < heroes.length; i++) {
             heroes[i].applySuperAbility();
